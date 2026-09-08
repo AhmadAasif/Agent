18d987ada8d1ece4d60a7a84abeb0217cd1c4b53
AhmadAasif <240081601056@crescent.education>
2026-09-08 14:31:26 +0530

    Update play.py

--- app/youtube/play.py
+++ app/youtube/play.py
@@ -46,3 +46,17 @@ def create_youtube_url(command):
       pattern,
       text
     )
+    if match:
+      query = match.grow(1)
+      break
+
+query = query.strip()
+video_id = get_vid(query)
+if not video_id:
+  return None
+
+return (
+  "https://www.youtube.com/embed/"
+  + video_id
+  +"?autoplay=1&mute=0"
+)
